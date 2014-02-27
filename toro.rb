66aecbd315ce32f092ca383efbea4af8e1ce5188
Barry Brinkley <baroldgene@gmail.com>
2014-02-26 21:20:06 -0500

    Fixes from the first round of test-flights.

--- toro.rb
+++ toro.rb
@@ -11,7 +11,8 @@ target = Colors.new
 drone  = Drone.new
 
 loop do
-  target_err = target.getMatch(Camera::FILE)
-  drone.move_to target_err.y
+  target_err = target.getMatch(Camera.get_last)
+  puts target_err.y
+  drone.move_to target_err.y * -1
   sleep DELAY_SEC
 end
